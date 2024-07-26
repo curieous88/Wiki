4668df3b96da9bc006be9ed9b3c9ecf0aba6a105
curieous88 <urvashimehta.com@gmail.com>
2024-07-26 23:20:44 +0530

    Templates

--- wiki/urls.py
+++ wiki/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns=[
     path("", views.index, name="index"),
     path("landing", views.landing, name="landing"),
-    path("<str:name>", views.greet, name="greet")
+    path("<str:name>", views.greet, name="greet"),
+    # path("homepage", views.homepage, name="homepage")
 ]
--- wiki/views.py
+++ wiki/views.py
@@ -3,10 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Wikipedia!")
+    return render(request,"wiki/homepage.html")
 
 def landing(request):
     return HttpResponse("page-2!")
 
 def greet(request, name):
     return HttpResponse(f"Hello, {name.capitalize()}!")
+
+# def homepage(request):
+#     return render(request, "wiki/homepage.html") 
