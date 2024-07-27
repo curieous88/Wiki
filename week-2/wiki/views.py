de3398c6a0ac360839d6a342cafd555fbc839745
curieous88 <urvashimehta.com@gmail.com>
2024-07-27 17:46:08 +0530

    changed folder structure

---
+++ week-2/wiki/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request,"wiki/homepage.html")
+
+def landing(request):
+    return HttpResponse("page-2!")
+
+# def greet(request, name):
+#     return HttpResponse(f"Hello, {name.capitalize()}!")
+
+def greet(request, name):
+    return render(request, "wiki/greet.html", {
+                  "name" : name.capitalize()
+    })
+
+def homepage(request):
+    return render(request, "wiki/homepage.html") 
