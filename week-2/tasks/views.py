1a13961f46ecb5c4f84868eac665c675d42c36be
curieous88 <urvashimehta.com@gmail.com>
2024-07-28 12:49:09 +0530

    adding tasks app

--- week-2/tasks/views.py
+++ week-2/tasks/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+tasks = ["Wash clothes", "make plant hanger", "order groceries"]
+
 # Create your views here.
+def index(request):
+     return render(request, "tasks/index.html", {
+          "tasks" : tasks
+          })
--- week-2/web50wiki/urls.py
+++ week-2/web50wiki/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('wiki/', include("wiki.urls")),
-    path('newyear/',include("newyear.urls"))
+    path('newyear/',include("newyear.urls")),
+    path('tasks/', include("tasks.urls"))
 ]
