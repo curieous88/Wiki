68aa57ac650e0f4e1422aacc104c1bf27d6b81df
curieous88 <urvashimehta.com@gmail.com>
2024-07-29 21:50:20 +0530

    {% url linking %}

--- week-2/tasks/views.py
+++ week-2/tasks/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render
 
 tasks = ["Wash clothes", "make plant hanger", "order groceries"]
@@ -7,3 +8,6 @@ def index(request):
      return render(request, "tasks/index.html", {
           "tasks" : tasks
           })
+
+def add(request):
+     return render(request, "tasks/add.html")
