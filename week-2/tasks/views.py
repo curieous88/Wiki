5993de382278b6af9b14d1c502aa2121fc17d055
curieous88 <urvashimehta.com@gmail.com>
2024-07-30 15:51:17 +0530

    redirect

--- week-2/tasks/views.py
+++ week-2/tasks/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 
-tasks = ["Wash clothes", "make plant hanger", "order groceries"]
+# tasks = ["Wash clothes", "make plant hanger", "order groceries"]
 
 class NewTaskForm(forms.Form):
      task = forms.CharField(label="New Task")
@@ -13,10 +13,10 @@ class NewTaskForm(forms.Form):
                    
 # Create your views here.
 def index(request):
-     if "tasks" is not in request.session:
+     if "tasks" not in request.session:
           request.session["tasks"] = []
      return render(request, "tasks/index.html", {
-          "tasks" : tasks
+          "tasks" : request.session["tasks"]
           })
 
 def add(request):
@@ -24,7 +24,8 @@ def add(request):
           form = NewTaskForm(request.POST)
           if form.is_valid():
                task = form.cleaned_data["task"]
-               tasks.append(task)
+               # tasks.append(task)
+               request.session["tasks"] += [task]
                return HttpResponseRedirect(reverse("tasks:index"))
           else:
                return render(request, "tasks/add.html", {
