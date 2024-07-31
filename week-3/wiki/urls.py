e2a3d8626219f8292fb99284d1d892f2c227f30e
curieous88 <urvashimehta.com@gmail.com>
2024-07-31 23:03:05 +0530

    Renaming folder

---
+++ week-3/wiki/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path("", views.index, name="index"),
+    path("landing", views.landing, name="landing"),
+    path("<str:name>", views.greet, name="greet"),
+    path("homepage", views.homepage, name="homepage")
+]
