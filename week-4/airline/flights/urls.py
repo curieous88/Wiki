ccb6f11e1fb8ed5d09dc1d9bc671fadf294c173a
curieous88 <urvashimehta.com@gmail.com>
2024-08-20 22:36:52 +0530

    Post the form

--- week-4/airline/flights/urls.py
+++ week-4/airline/flights/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="flights"),
     path("<int:flight_id>", views.flightinfo, name="flightinfo"),
+    path("<int:flight_id>/add_passengers_to_flight", views.add_passengers_to_flight, name="add_passengers_to_flight")
 ]
--- week-4/airline/flights/views.py
+++ week-4/airline/flights/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Flight, Airport, Passenger
 from django.http import HttpResponseRedirect
 
+
 # Create your views here.
 
 def index(request):       
@@ -14,15 +15,19 @@ def flightinfo(request,flight_id):
       flight=Flight.objects.get(id=flight_id)
       return render(request, "flights/flightinfo.html", {
          "flightinfo": flight,
-         "passengers_all": Passenger.objects.all(),
-         "passengers_in_flight": flight.passengers.all()
+         "passengers_all": Passenger.objects.all().order_by('first'),
+         "passengers_in_flight": flight.passengers.all().order_by('first')
        })
       
-# def book(request, flight_id):
-#   if request.method == "POST":
-#     flight=Flight.objects.get(flight_id)
-#     return HttpResponseRedirect
-    
-      
+def add_passengers_to_flight(request, flight_id):
+    flight=Flight.objects.get(flight_id)
+    if request.method == "POST":
+      selected_passenger_ids = request.POST.getlist('add_passengers')
+      add_passengers = Passenger.objects.filter(id__in=selected_passenger_ids)
+      for passenger in add_passengers:
+        flight.passengers.add(passenger)      
+      return HttpResponseRedirect(reverse("flightinfo", args=(flight.id,)))
+    else:
+      return HttpResponseNotAllowed(['POST'])
 
 
