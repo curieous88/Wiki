a009adf0534d3b91afda1b1babc74742c8a30346
curieous88 <urvashimehta.com@gmail.com>
2024-08-22 21:03:27 +0530

    reverse template

--- week-4/airline/flights/views.py
+++ week-4/airline/flights/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Flight, Airport, Passenger
 from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 # Create your views here.
@@ -13,21 +14,30 @@ def index(request):
 
 def flightinfo(request,flight_id):  
       flight=Flight.objects.get(id=flight_id)
+      print("flight id is:" , flight_id)  # This should output the correct ID  
       return render(request, "flights/flightinfo.html", {
          "flightinfo": flight,
          "passengers_all": Passenger.objects.all().order_by('first'),
-         "passengers_in_flight": flight.passengers.all().order_by('first')
+         "passengers_in_flight": flight.passengers.all().order_by('first'),
+         "flight_id": flight_id
        })
       
 def add_passengers_to_flight(request, flight_id):
-    flight=Flight.objects.get(flight_id)
-    if request.method == "POST":
+    flight=Flight.objects.get(id=flight_id)
+    print("ftttfffflight id is:" , flight_id, flight.id, flight)  # This should output the correct ID  
+    if request.method == 'POST':
       selected_passenger_ids = request.POST.getlist('add_passengers')
       add_passengers = Passenger.objects.filter(id__in=selected_passenger_ids)
       for passenger in add_passengers:
-        flight.passengers.add(passenger)      
-      return HttpResponseRedirect(reverse("flightinfo", args=(flight.id,)))
+        flight.passengers.add(passenger)    
+        print("flight id is:" , flight_id)  # This should output the correct ID  
+      return HttpResponseRedirect(reverse("add_passengers_to_flight", args=[flight_id]))
     else:
-      return HttpResponseNotAllowed(['POST'])
-
+      return render(request, "flights/flightinfo.html", {
+         "flightinfo": flight,
+         "passengers_all": Passenger.objects.all().order_by('first'),
+         "passengers_in_flight": flight.passengers.all().order_by('first'),
+         "flight_id": flight_id
+       })
+      
 
