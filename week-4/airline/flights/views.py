ab64430a4aa192fe7c04cd0fe278a8c1f655d2b9
curieous88 <urvashimehta.com@gmail.com>
2024-08-23 21:31:39 +0530

    trying to pass "id"

--- week-4/airline/flights/views.py
+++ week-4/airline/flights/views.py
@@ -26,12 +26,14 @@ def add_passengers_to_flight(request, flight_id):
     flight=Flight.objects.get(id=flight_id)
     print("ftttfffflight id is:" , flight_id, flight.id, flight)  # This should output the correct ID  
     if request.method == 'POST':
-      selected_passenger_ids = request.POST.getlist('add_passengers')
-      add_passengers = Passenger.objects.filter(id__in=selected_passenger_ids)
-      for passenger in add_passengers:
-        flight.passengers.add(passenger)    
-        print("flight id is:" , flight_id)  # This should output the correct ID  
-      return HttpResponseRedirect(reverse("add_passengers_to_flight", args=[flight_id]))
+      selected_passenger = int(request.POST["passenger"])
+      print("list id is:" , selected_passenger)  # This should output the correct ID  
+      add_passengers = Passenger.objects.get(pk=selected_passenger)
+      print("passen id is:" , add_passengers)  # This should output the correct ID 
+      passenger.flights.add(flight) 
+      # for passenger in add_passengers:
+      #   flight.passengers.add(passenger)    
+      return HttpResponseRedirect(reverse("add_passengers_to_flight", args=[flight.id]))
     else:
       return render(request, "flights/flightinfo.html", {
          "flightinfo": flight,
