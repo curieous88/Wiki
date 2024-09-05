09f62fa066c1c2eac02a845d5ddb8bbb5407deec
curieous88 <urvashimehta.com@gmail.com>
2024-09-05 18:52:54 +0530

    urlpatterns

--- week-4/airline/users/urls.py
+++ week-4/airline/users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
 urlpatterns = [
-    
+    path('', views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout")
 ]
