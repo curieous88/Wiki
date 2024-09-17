6ffe086a28a5abe5f7271aa6c125f4422c528703
curieous88 <urvashimehta.com@gmail.com>
2024-09-17 21:44:29 +0530

    users views.py

--- week-4/airline/users/views.py
+++ week-4/airline/users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
+def index(request):
+    # If no user is signed in, return to login page:
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reversed("login"))
+    return render(request, "users/user.html")
+
+def login_view(request):
+    return render(request, "users/login.html")
+
+def logout_view(request):
+    # Pass is a simple way to tell python to do nothing.
+    pass
